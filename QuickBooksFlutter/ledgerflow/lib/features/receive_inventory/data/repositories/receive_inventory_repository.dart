5ab6caf1e3aba5e70f1b1f70eb4fe7fbfc4e679b
khaledzayeds <khaledtornado@gmail.com>
2026-05-01 09:31:49 +0300

    Fill receive inventory repository

--- QuickBooksFlutter/ledgerflow/lib/features/receive_inventory/data/repositories/receive_inventory_repository.dart
+++ QuickBooksFlutter/ledgerflow/lib/features/receive_inventory/data/repositories/receive_inventory_repository.dart
@@ -1,1 +1,26 @@
-﻿// receive_inventory_repository.dart
+import '../../../../core/api/api_result.dart';
+import '../datasources/receive_inventory_remote_datasource.dart';
+import '../models/create_receive_inventory_dto.dart';
+import '../models/receive_inventory_model.dart';
+
+class ReceiveInventoryRepository {
+  ReceiveInventoryRepository(this._remote);
+
+  final ReceiveInventoryRemoteDatasource _remote;
+
+  Future<ApiResult<List<ReceiveInventoryModel>>> getAll({
+    String? purchaseOrderId,
+  }) {
+    return _remote.getAll(purchaseOrderId: purchaseOrderId);
+  }
+
+  Future<ApiResult<ReceiveInventoryModel>> getById(String id) {
+    return _remote.getById(id);
+  }
+
+  Future<ApiResult<ReceiveInventoryModel>> create(
+    CreateReceiveInventoryDto dto,
+  ) {
+    return _remote.create(dto);
+  }
+}
