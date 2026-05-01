5f341e56754b2b3db18d8cf46869c8ff671c36a9
khaledzayeds <khaledtornado@gmail.com>
2026-05-01 09:38:22 +0300

    Add receive inventory providers

--- QuickBooksFlutter/ledgerflow/lib/features/receive_inventory/providers/receive_inventory_provider.dart
+++ QuickBooksFlutter/ledgerflow/lib/features/receive_inventory/providers/receive_inventory_provider.dart
@@ -1,1 +1,34 @@
-﻿// receive_inventory_provider.dart
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import '../data/datasources/receive_inventory_remote_datasource.dart';
+import '../data/models/receive_inventory_model.dart';
+import '../data/repositories/receive_inventory_repository.dart';
+
+final receiveInventoryDatasourceProvider = Provider(
+  (_) => ReceiveInventoryRemoteDatasource(),
+);
+
+final receiveInventoryRepoProvider = Provider(
+  (ref) => ReceiveInventoryRepository(
+    ref.read(receiveInventoryDatasourceProvider),
+  ),
+);
+
+final receiveInventoryListProvider = FutureProvider(
+  (ref) async {
+    final result = await ref.read(receiveInventoryRepoProvider).getAll();
+    return result.when(
+      success: (data) => data,
+      failure: (error) => throw error,
+    );
+  },
+);
+
+final receiveInventoryDetailsProvider =
+    FutureProvider.family<ReceiveInventoryModel, String>((ref, id) async {
+  final result = await ref.read(receiveInventoryRepoProvider).getById(id);
+  return result.when(
+    success: (data) => data,
+    failure: (error) => throw error,
+  );
+});
