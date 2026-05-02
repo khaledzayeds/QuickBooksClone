f635c56eac6df0d3774f29914fb7c3034ec7121b
khaledzayeds <khaledtornado@gmail.com>
2026-05-02 10:50:48 +0300

    Add sales receipts providers

--- QuickBooksFlutter/ledgerflow/lib/features/sales_receipts/providers/sales_receipts_provider.dart
+++ QuickBooksFlutter/ledgerflow/lib/features/sales_receipts/providers/sales_receipts_provider.dart
@@ -1,1 +1,47 @@
-﻿// sales_receipts_provider.dart
+// sales_receipts_provider.dart
+
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import '../../../core/api/api_client.dart';
+import '../data/datasources/sales_receipts_remote_datasource.dart';
+import '../data/models/sales_receipt_models.dart';
+import '../data/repositories/sales_receipts_repository.dart';
+
+final salesReceiptsDatasourceProvider = Provider<SalesReceiptsRemoteDatasource>(
+  (ref) => SalesReceiptsRemoteDatasource(ApiClient.instance),
+);
+
+final salesReceiptsRepositoryProvider = Provider<SalesReceiptsRepository>(
+  (ref) => SalesReceiptsRepository(ref.watch(salesReceiptsDatasourceProvider)),
+);
+
+final salesReceiptsProvider =
+    AsyncNotifierProvider<SalesReceiptsNotifier, List<SalesReceiptModel>>(
+  SalesReceiptsNotifier.new,
+);
+
+class SalesReceiptsNotifier extends AsyncNotifier<List<SalesReceiptModel>> {
+  @override
+  Future<List<SalesReceiptModel>> build() => _fetch();
+
+  Future<List<SalesReceiptModel>> _fetch() async {
+    final result = await ref.read(salesReceiptsRepositoryProvider).getAll();
+    return result.when(
+      success: (data) => data,
+      failure: (error) => throw error,
+    );
+  }
+
+  Future<void> refresh() async {
+    state = const AsyncLoading();
+    state = await AsyncValue.guard(_fetch);
+  }
+}
+
+final salesReceiptDetailsProvider =
+    FutureProvider.family<SalesReceiptModel, String>((ref, id) async {
+  final result = await ref.read(salesReceiptsRepositoryProvider).getById(id);
+  return result.when(
+    success: (data) => data,
+    failure: (error) => throw error,
+  );
+});
