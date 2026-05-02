966b695d6ab96e7031548fe4af485d242ecfe374
khaledzayeds <khaledtornado@gmail.com>
2026-05-02 10:50:08 +0300

    Add sales receipts repository

--- QuickBooksFlutter/ledgerflow/lib/features/sales_receipts/data/repositories/sales_receipts_repository.dart
+++ QuickBooksFlutter/ledgerflow/lib/features/sales_receipts/data/repositories/sales_receipts_repository.dart
@@ -1,1 +1,34 @@
-﻿// sales_receipts_repository.dart
+// sales_receipts_repository.dart
+
+import '../../../../core/api/api_result.dart';
+import '../datasources/sales_receipts_remote_datasource.dart';
+import '../models/sales_receipt_models.dart';
+
+class SalesReceiptsRepository {
+  SalesReceiptsRepository(this._datasource);
+  final SalesReceiptsRemoteDatasource _datasource;
+
+  Future<ApiResult<List<SalesReceiptModel>>> getAll({
+    String? search,
+    String? customerId,
+    bool includeVoid = false,
+    int page = 1,
+    int pageSize = 25,
+  }) =>
+      _datasource.getAll(
+        search: search,
+        customerId: customerId,
+        includeVoid: includeVoid,
+        page: page,
+        pageSize: pageSize,
+      );
+
+  Future<ApiResult<SalesReceiptModel>> getById(String id) =>
+      _datasource.getById(id);
+
+  Future<ApiResult<SalesReceiptModel>> create(CreateSalesReceiptDto dto) =>
+      _datasource.create(dto);
+
+  Future<ApiResult<SalesReceiptModel>> voidReceipt(String id) =>
+      _datasource.voidReceipt(id);
+}
