66f3e517416a7084e32f7b43d7f1c714ff260042
khaledzayeds <khaledtornado@gmail.com>
2026-05-03 16:25:31 +0300

    Add payments repository

--- QuickBooksFlutter/ledgerflow/lib/features/payments/data/repositories/payments_repository.dart
+++ QuickBooksFlutter/ledgerflow/lib/features/payments/data/repositories/payments_repository.dart
@@ -1,1 +1,34 @@
-﻿// payments_repository.dart
+// payments_repository.dart
+
+import '../../../../core/api/api_result.dart';
+import '../datasources/payments_remote_datasource.dart';
+import '../models/payment_model.dart';
+
+class PaymentsRepository {
+  PaymentsRepository(this._datasource);
+
+  final PaymentsRemoteDatasource _datasource;
+
+  Future<ApiResult<List<PaymentModel>>> getAll({
+    String? search,
+    String? customerId,
+    String? invoiceId,
+    bool includeVoid = false,
+    int page = 1,
+    int pageSize = 25,
+  }) =>
+      _datasource.getAll(
+        search: search,
+        customerId: customerId,
+        invoiceId: invoiceId,
+        includeVoid: includeVoid,
+        page: page,
+        pageSize: pageSize,
+      );
+
+  Future<ApiResult<PaymentModel>> getById(String id) => _datasource.getById(id);
+
+  Future<ApiResult<PaymentModel>> create(CreatePaymentDto dto) => _datasource.create(dto);
+
+  Future<ApiResult<PaymentModel>> voidPayment(String id) => _datasource.voidPayment(id);
+}
