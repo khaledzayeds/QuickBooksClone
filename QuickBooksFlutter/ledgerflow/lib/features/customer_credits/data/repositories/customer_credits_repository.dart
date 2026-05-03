2cbd3a580451e11b758b81707f4c7ff5cb87ad61
khaledzayeds <khaledtornado@gmail.com>
2026-05-03 18:26:41 +0300

    Add customer credits repository

--- QuickBooksFlutter/ledgerflow/lib/features/customer_credits/data/repositories/customer_credits_repository.dart
+++ QuickBooksFlutter/ledgerflow/lib/features/customer_credits/data/repositories/customer_credits_repository.dart
@@ -1,1 +1,34 @@
-﻿// customer_credits_repository.dart
+// customer_credits_repository.dart
+
+import '../../../../core/api/api_result.dart';
+import '../datasources/customer_credits_remote_datasource.dart';
+import '../models/customer_credit_model.dart';
+
+class CustomerCreditsRepository {
+  CustomerCreditsRepository(this._datasource);
+
+  final CustomerCreditsRemoteDatasource _datasource;
+
+  Future<ApiResult<List<CustomerCreditModel>>> getAll({
+    String? search,
+    String? customerId,
+    int? action,
+    bool includeVoid = false,
+    int page = 1,
+    int pageSize = 25,
+  }) =>
+      _datasource.getAll(
+        search: search,
+        customerId: customerId,
+        action: action,
+        includeVoid: includeVoid,
+        page: page,
+        pageSize: pageSize,
+      );
+
+  Future<ApiResult<CustomerCreditModel>> getById(String id) => _datasource.getById(id);
+
+  Future<ApiResult<CustomerCreditModel>> create(CreateCustomerCreditDto dto) => _datasource.create(dto);
+
+  Future<ApiResult<CustomerCreditModel>> post(String id) => _datasource.post(id);
+}
