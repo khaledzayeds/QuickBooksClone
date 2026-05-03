9606accfe251fefc1b5b1f3d2a09849b724a284f
khaledzayeds <khaledtornado@gmail.com>
2026-05-03 16:49:52 +0300

    Add purchase returns repository

--- QuickBooksFlutter/ledgerflow/lib/features/purchase_returns/data/repositories/purchase_returns_repository.dart
+++ QuickBooksFlutter/ledgerflow/lib/features/purchase_returns/data/repositories/purchase_returns_repository.dart
@@ -1,1 +1,34 @@
-﻿// purchase_returns_repository.dart
+// purchase_returns_repository.dart
+
+import '../../../../core/api/api_result.dart';
+import '../datasources/purchase_returns_remote_datasource.dart';
+import '../models/purchase_return_model.dart';
+
+class PurchaseReturnsRepository {
+  PurchaseReturnsRepository(this._datasource);
+
+  final PurchaseReturnsRemoteDatasource _datasource;
+
+  Future<ApiResult<List<PurchaseReturnModel>>> getAll({
+    String? search,
+    String? purchaseBillId,
+    String? vendorId,
+    bool includeVoid = false,
+    int page = 1,
+    int pageSize = 25,
+  }) =>
+      _datasource.getAll(
+        search: search,
+        purchaseBillId: purchaseBillId,
+        vendorId: vendorId,
+        includeVoid: includeVoid,
+        page: page,
+        pageSize: pageSize,
+      );
+
+  Future<ApiResult<PurchaseReturnModel>> getById(String id) => _datasource.getById(id);
+
+  Future<ApiResult<PurchaseReturnModel>> create(CreatePurchaseReturnDto dto) => _datasource.create(dto);
+
+  Future<ApiResult<PurchaseReturnModel>> voidReturn(String id) => _datasource.voidReturn(id);
+}
