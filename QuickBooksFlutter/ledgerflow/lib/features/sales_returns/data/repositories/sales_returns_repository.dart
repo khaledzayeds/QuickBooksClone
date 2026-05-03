a357a152386b10ff409949f0052532f6801a1854
khaledzayeds <khaledtornado@gmail.com>
2026-05-03 16:42:10 +0300

    Add sales returns repository

--- QuickBooksFlutter/ledgerflow/lib/features/sales_returns/data/repositories/sales_returns_repository.dart
+++ QuickBooksFlutter/ledgerflow/lib/features/sales_returns/data/repositories/sales_returns_repository.dart
@@ -1,1 +1,36 @@
-﻿// sales_returns_repository.dart
+// sales_returns_repository.dart
+
+import '../../../../core/api/api_result.dart';
+import '../datasources/sales_returns_remote_datasource.dart';
+import '../models/sales_return_model.dart';
+
+class SalesReturnsRepository {
+  SalesReturnsRepository(this._datasource);
+
+  final SalesReturnsRemoteDatasource _datasource;
+
+  Future<ApiResult<List<SalesReturnModel>>> getAll({
+    String? search,
+    String? invoiceId,
+    String? customerId,
+    bool includeVoid = false,
+    int page = 1,
+    int pageSize = 25,
+  }) =>
+      _datasource.getAll(
+        search: search,
+        invoiceId: invoiceId,
+        customerId: customerId,
+        includeVoid: includeVoid,
+        page: page,
+        pageSize: pageSize,
+      );
+
+  Future<ApiResult<SalesReturnModel>> getById(String id) => _datasource.getById(id);
+
+  Future<ApiResult<SalesReturnModel>> create(CreateSalesReturnDto dto) => _datasource.create(dto);
+
+  Future<ApiResult<SalesReturnModel>> post(String id) => _datasource.post(id);
+
+  Future<ApiResult<SalesReturnModel>> voidReturn(String id) => _datasource.voidReturn(id);
+}
