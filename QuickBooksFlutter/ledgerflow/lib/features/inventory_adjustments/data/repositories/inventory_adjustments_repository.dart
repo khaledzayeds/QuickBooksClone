3f4535fd750ff583b9f2e00fe099ab4e7d47bd7d
khaledzayeds <khaledtornado@gmail.com>
2026-05-03 17:32:42 +0300

    Add inventory adjustments repository

--- QuickBooksFlutter/ledgerflow/lib/features/inventory_adjustments/data/repositories/inventory_adjustments_repository.dart
+++ QuickBooksFlutter/ledgerflow/lib/features/inventory_adjustments/data/repositories/inventory_adjustments_repository.dart
@@ -1,1 +1,30 @@
-﻿// inventory_adjustments_repository.dart
+// inventory_adjustments_repository.dart
+
+import '../../../../core/api/api_result.dart';
+import '../datasources/inventory_adjustments_remote_datasource.dart';
+import '../models/inventory_adjustment_model.dart';
+
+class InventoryAdjustmentsRepository {
+  InventoryAdjustmentsRepository(this._datasource);
+
+  final InventoryAdjustmentsRemoteDatasource _datasource;
+
+  Future<ApiResult<List<InventoryAdjustmentModel>>> getAll({
+    String? search,
+    String? itemId,
+    bool includeVoid = false,
+    int page = 1,
+    int pageSize = 25,
+  }) =>
+      _datasource.getAll(
+        search: search,
+        itemId: itemId,
+        includeVoid: includeVoid,
+        page: page,
+        pageSize: pageSize,
+      );
+
+  Future<ApiResult<InventoryAdjustmentModel>> getById(String id) => _datasource.getById(id);
+
+  Future<ApiResult<InventoryAdjustmentModel>> create(CreateInventoryAdjustmentDto dto) => _datasource.create(dto);
+}
