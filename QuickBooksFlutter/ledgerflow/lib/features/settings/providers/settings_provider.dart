98f22a4b00c0ab7149f74131fc999936ffe9ecd8
khaledzayeds <khaledtornado@gmail.com>
2026-05-04 10:30:45 +0300

    Add settings providers

--- QuickBooksFlutter/ledgerflow/lib/features/settings/providers/settings_provider.dart
+++ QuickBooksFlutter/ledgerflow/lib/features/settings/providers/settings_provider.dart
@@ -1,1 +1,14 @@
-﻿// settings_provider.dart
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import '../data/models/settings_models.dart';
+import '../data/settings_repository.dart';
+
+final settingsRepositoryProvider = Provider<SettingsRepository>((ref) => SettingsRepository());
+
+final runtimeSettingsProvider = FutureProvider<RuntimeSettingsModel>((ref) async {
+  return ref.watch(settingsRepositoryProvider).getRuntime();
+});
+
+final companySettingsProvider = FutureProvider<CompanySettingsModel?>((ref) async {
+  return ref.watch(settingsRepositoryProvider).getCompany();
+});
