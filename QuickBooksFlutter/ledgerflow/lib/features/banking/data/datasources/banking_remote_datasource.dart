de7b8dc11e23a64a0364461fb983ba19c53bd345
khaledzayeds <khaledtornado@gmail.com>
2026-05-06 09:17:33 +0300

    Add Flutter banking remote datasource

---
+++ QuickBooksFlutter/ledgerflow/lib/features/banking/data/datasources/banking_remote_datasource.dart
@@ -0,0 +1,47 @@
+// banking_remote_datasource.dart
+
+import 'package:dio/dio.dart';
+
+import '../../../../core/api/api_client.dart';
+import '../../../../core/api/api_result.dart';
+import '../../../../core/utils/error_handler.dart';
+import '../models/banking_models.dart';
+
+class BankingRemoteDatasource {
+  BankingRemoteDatasource(this._client);
+
+  final ApiClient _client;
+
+  Future<ApiResult<List<BankAccountModel>>> getAccounts() async {
+    try {
+      final response = await _client.get<List<dynamic>>('/api/banking/accounts');
+      final list = (response.data ?? const [])
+          .map((json) => BankAccountModel.fromJson(json as Map<String, dynamic>))
+          .toList();
+      return Success(list);
+    } on DioException catch (error) {
+      return Failure(parseError(error));
+    }
+  }
+
+  Future<ApiResult<BankRegisterResponseModel>> getRegister(String accountId) async {
+    try {
+      final response = await _client.get<Map<String, dynamic>>(
+        '/api/banking/register',
+        queryParameters: {'accountId': accountId},
+      );
+      return Success(BankRegisterResponseModel.fromJson(response.data!));
+    } on DioException catch (error) {
+      return Failure(parseError(error));
+    }
+  }
+
+  Future<ApiResult<void>> createTransfer(CreateBankTransferDto dto) async {
+    try {
+      await _client.post<Map<String, dynamic>>('/api/banking/transfers', data: dto.toJson());
+      return const Success(null);
+    } on DioException catch (error) {
+      return Failure(parseError(error));
+    }
+  }
+}
