381f285dd7b7084b15b426f4b0974bb37c3d5123
khaledzayeds <khaledtornado@gmail.com>
2026-05-08 18:28:45 +0300

    Add Flutter time tracking summary report model

--- QuickBooksFlutter/ledgerflow/lib/features/time_tracking/providers/time_entries_provider.dart
+++ QuickBooksFlutter/ledgerflow/lib/features/time_tracking/providers/time_entries_provider.dart
@@ -13,6 +13,11 @@ final timeEntryLookupsProvider = FutureProvider.autoDispose<TimeEntryLookups>((r
   return TimeEntryLookups.fromJson(response.data!);
 });
 
+final timeEntrySummaryReportProvider = FutureProvider.autoDispose<TimeEntrySummaryReport>((ref) async {
+  final response = await ApiClient.instance.get<Map<String, dynamic>>('/api/time-entries/reports/summary');
+  return TimeEntrySummaryReport.fromJson(response.data!);
+});
+
 final timeEntriesCommandsProvider = Provider<TimeEntriesCommands>((ref) => TimeEntriesCommands(ref));
 
 class TimeEntriesCommands {
@@ -43,17 +48,17 @@ class TimeEntriesCommands {
         'isBillable': isBillable,
       },
     );
-    ref.invalidate(timeEntriesProvider);
+    _invalidateTime(ref);
   }
 
   Future<void> approve(String id) async {
     await ApiClient.instance.post<Map<String, dynamic>>('/api/time-entries/$id/approve');
-    ref.invalidate(timeEntriesProvider);
+    _invalidateTime(ref);
   }
 
   Future<void> markBillable(String id) async {
     await ApiClient.instance.post<Map<String, dynamic>>('/api/time-entries/$id/mark-billable');
-    ref.invalidate(timeEntriesProvider);
+    _invalidateTime(ref);
   }
 
   Future<void> markInvoiced(String id, {String? invoiceId}) async {
@@ -61,12 +66,17 @@ class TimeEntriesCommands {
       '/api/time-entries/$id/mark-invoiced-with-link',
       data: {'invoiceId': invoiceId},
     );
-    ref.invalidate(timeEntriesProvider);
+    _invalidateTime(ref);
   }
 
   Future<void> voidEntry(String id) async {
     await ApiClient.instance.patch<Map<String, dynamic>>('/api/time-entries/$id/void');
+    _invalidateTime(ref);
+  }
+
+  void _invalidateTime(Ref ref) {
     ref.invalidate(timeEntriesProvider);
+    ref.invalidate(timeEntrySummaryReportProvider);
   }
 }
 
@@ -190,6 +200,148 @@ class TimeEntry {
       );
 }
 
+class TimeEntrySummaryReport {
+  const TimeEntrySummaryReport({
+    required this.fromDate,
+    required this.toDate,
+    required this.entryCount,
+    required this.totalHours,
+    required this.billableHours,
+    required this.nonBillableHours,
+    required this.billableNotInvoicedHours,
+    required this.byStatus,
+    required this.byPerson,
+    required this.byCustomer,
+    required this.billableQueue,
+  });
+
+  final DateTime? fromDate;
+  final DateTime? toDate;
+  final int entryCount;
+  final double totalHours;
+  final double billableHours;
+  final double nonBillableHours;
+  final double billableNotInvoicedHours;
+  final List<TimeEntrySummaryByStatus> byStatus;
+  final List<TimeEntrySummaryByPerson> byPerson;
+  final List<TimeEntrySummaryByCustomer> byCustomer;
+  final List<BillableTimeQueueItem> billableQueue;
+
+  factory TimeEntrySummaryReport.fromJson(Map<String, dynamic> json) => TimeEntrySummaryReport(
+        fromDate: _parseNullableDate(json['fromDate']),
+        toDate: _parseNullableDate(json['toDate']),
+        entryCount: JsonUtils.asInt(json['entryCount']),
+        totalHours: JsonUtils.asDouble(json['totalHours']),
+        billableHours: JsonUtils.asDouble(json['billableHours']),
+        nonBillableHours: JsonUtils.asDouble(json['nonBillableHours']),
+        billableNotInvoicedHours: JsonUtils.asDouble(json['billableNotInvoicedHours']),
+        byStatus: JsonUtils.asList(json['byStatus'], (row) => TimeEntrySummaryByStatus.fromJson(row)),
+        byPerson: JsonUtils.asList(json['byPerson'], (row) => TimeEntrySummaryByPerson.fromJson(row)),
+        byCustomer: JsonUtils.asList(json['byCustomer'], (row) => TimeEntrySummaryByCustomer.fromJson(row)),
+        billableQueue: JsonUtils.asList(json['billableQueue'], (row) => BillableTimeQueueItem.fromJson(row)),
+      );
+}
+
+class TimeEntrySummaryByStatus {
+  const TimeEntrySummaryByStatus({required this.status, required this.entryCount, required this.totalHours, required this.billableHours});
+
+  final TimeEntryStatus status;
+  final int entryCount;
+  final double totalHours;
+  final double billableHours;
+
+  factory TimeEntrySummaryByStatus.fromJson(Map<String, dynamic> json) => TimeEntrySummaryByStatus(
+        status: _status(json['status']),
+        entryCount: JsonUtils.asInt(json['entryCount']),
+        totalHours: JsonUtils.asDouble(json['totalHours']),
+        billableHours: JsonUtils.asDouble(json['billableHours']),
+      );
+}
+
+class TimeEntrySummaryByPerson {
+  const TimeEntrySummaryByPerson({required this.personName, required this.entryCount, required this.totalHours, required this.billableHours, required this.invoicedHours});
+
+  final String personName;
+  final int entryCount;
+  final double totalHours;
+  final double billableHours;
+  final double invoicedHours;
+
+  factory TimeEntrySummaryByPerson.fromJson(Map<String, dynamic> json) => TimeEntrySummaryByPerson(
+        personName: JsonUtils.asString(json['personName']),
+        entryCount: JsonUtils.asInt(json['entryCount']),
+        totalHours: JsonUtils.asDouble(json['totalHours']),
+        billableHours: JsonUtils.asDouble(json['billableHours']),
+        invoicedHours: JsonUtils.asDouble(json['invoicedHours']),
+      );
+}
+
+class TimeEntrySummaryByCustomer {
+  const TimeEntrySummaryByCustomer({
+    required this.customerId,
+    required this.customerName,
+    required this.entryCount,
+    required this.totalHours,
+    required this.billableHours,
+    required this.billableNotInvoicedHours,
+  });
+
+  final String? customerId;
+  final String customerName;
+  final int entryCount;
+  final double totalHours;
+  final double billableHours;
+  final double billableNotInvoicedHours;
+
+  factory TimeEntrySummaryByCustomer.fromJson(Map<String, dynamic> json) => TimeEntrySummaryByCustomer(
+        customerId: JsonUtils.asNullableString(json['customerId']),
+        customerName: JsonUtils.asString(json['customerName']),
+        entryCount: JsonUtils.asInt(json['entryCount']),
+        totalHours: JsonUtils.asDouble(json['totalHours']),
+        billableHours: JsonUtils.asDouble(json['billableHours']),
+        billableNotInvoicedHours: JsonUtils.asDouble(json['billableNotInvoicedHours']),
+      );
+}
+
+class BillableTimeQueueItem {
+  const BillableTimeQueueItem({
+    required this.id,
+    required this.workDate,
+    required this.personName,
+    required this.hours,
+    required this.activity,
+    required this.customerId,
+    required this.customerName,
+    required this.serviceItemId,
+    required this.serviceItemName,
+    required this.status,
+  });
+
+  final String id;
+  final DateTime workDate;
+  final String personName;
+  final double hours;
+  final String activity;
+  final String customerId;
+  final String customerName;
+  final String serviceItemId;
+  final String serviceItemName;
+  final TimeEntryStatus status;
+
+  factory BillableTimeQueueItem.fromJson(Map<String, dynamic> json) => BillableTimeQueueItem(
+        id: JsonUtils.asString(json['id']),
+        workDate: _parseDate(json['workDate']),
+        personName: JsonUtils.asString(json['personName']),
+        hours: JsonUtils.asDouble(json['hours']),
+        activity: JsonUtils.asString(json['activity']),
+        customerId: JsonUtils.asString(json['customerId']),
+        customerName: JsonUtils.asString(json['customerName']),
+        serviceItemId: JsonUtils.asString(json['serviceItemId']),
+        serviceItemName: JsonUtils.asString(json['serviceItemName']),
+        status: _status(json['status']),
+      );
+}
+
 enum TimeEntryStatus { open, approved, billable, invoiced, voided }
 
 TimeEntryStatus _status(dynamic value) {
@@ -203,5 +355,14 @@ TimeEntryStatus _status(dynamic value) {
   };
 }
 
+String timeEntryStatusLabel(TimeEntryStatus status) => switch (status) {
+      TimeEntryStatus.open => 'Open',
+      TimeEntryStatus.approved => 'Approved',
+      TimeEntryStatus.billable => 'Billable',
+      TimeEntryStatus.invoiced => 'Invoiced',
+      TimeEntryStatus.voided => 'Void',
+    };
+
 String _dateOnly(DateTime date) => '${date.year}-${date.month.toString().padLeft(2, '0')}-${date.day.toString().padLeft(2, '0')}';
 DateTime _parseDate(dynamic value) => DateTime.tryParse(value?.toString() ?? '') ?? DateTime.now();
+DateTime? _parseNullableDate(dynamic value) => value == null ? null : DateTime.tryParse(value.toString());
