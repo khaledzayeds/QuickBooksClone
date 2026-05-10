324484b686698a49a026767302d439c3139b63fb
khaledzayeds <khaledtornado@gmail.com>
2026-05-10 11:49:16 +0300

    Add Invoice body panel

---
+++ QuickBooksFlutter/ledgerflow/lib/features/invoices/widgets/invoice_body_panel.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+
+import '../../purchase_orders/data/models/order_line_entry.dart';
+import '../../transactions/widgets/transaction_models.dart';
+import 'invoice_header_panel.dart';
+import 'invoice_lines_panel.dart';
+
+class InvoiceBodyPanel extends StatelessWidget {
+  const InvoiceBodyPanel({
+    super.key,
+    required this.customerField,
+    required this.invoiceNumberField,
+    required this.invoiceDateField,
+    required this.dueDateField,
+    required this.billingTermsField,
+    required this.memoField,
+    required this.lines,
+    required this.totals,
+    required this.onAddLine,
+    required this.onLinesChanged,
+  });
+
+  final Widget customerField;
+  final Widget invoiceNumberField;
+  final Widget invoiceDateField;
+  final Widget dueDateField;
+  final Widget billingTermsField;
+  final Widget memoField;
+  final List<TransactionLineEntry> lines;
+  final TransactionTotalsUiModel totals;
+  final VoidCallback onAddLine;
+  final VoidCallback onLinesChanged;
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+
+    return Column(
+      children: [
+        InvoiceHeaderPanel(
+          customerField: customerField,
+          invoiceNumberField: invoiceNumberField,
+          invoiceDateField: invoiceDateField,
+          dueDateField: dueDateField,
+          billingTermsField: billingTermsField,
+          memoField: memoField,
+        ),
+        Divider(height: 1, color: theme.dividerColor),
+        InvoiceLinesPanel(
+          lines: lines,
+          totals: totals,
+          onAddLine: onAddLine,
+          onLinesChanged: onLinesChanged,
+        ),
+      ],
+    );
+  }
+}
