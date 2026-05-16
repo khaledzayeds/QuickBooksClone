f45edad90733ea9f9bf4deaea59718b5ad866609
khaledzayeds <khaledtornado@gmail.com>
2026-05-16 23:30:48 +0300

    chore: lock offline app constants

--- QuickBooksFlutter/ledgerflow/lib/core/constants/app_constants.dart
+++ QuickBooksFlutter/ledgerflow/lib/core/constants/app_constants.dart
@@ -1,12 +1,24 @@
-﻿// app_constants.dart
+// app_constants.dart
 // core/constants/app_constants.dart
 
 class AppConstants {
-  // الـ base URL من الـ FRONTEND_INTEGRATION_GUIDE
+  AppConstants._();
+
+  static const appName = 'LedgerFlow';
+  static const appEdition = 'Offline';
+  static const appDisplayName = '$appName $appEdition';
+
+  /// Internal local API endpoint used by the desktop offline build.
+  ///
+  /// The user should not need to start or configure this endpoint manually;
+  /// a desktop launcher/host will own that responsibility.
   static const defaultBaseUrl = 'http://localhost:5014';
 
-  // يتغير حسب الـ connection profile (Local / LAN / Hosted)
-  static const localUrl = 'http://localhost:5014';
-  static const lanUrl = 'http://192.168.1.x:5014'; // يتغير
-  static const hostedUrl = 'https://your-server.com';
+  static const dataRootFolderName = 'LedgerFlow';
+  static const companiesFolderName = 'Companies';
+  static const backupsFolderName = 'Backups';
+  static const templatesFolderName = 'Templates';
+
+  static const defaultCompanyDatabaseFileName = 'ledgerflow.db';
+  static const companyFileExtension = '.ledgerflow';
 }
