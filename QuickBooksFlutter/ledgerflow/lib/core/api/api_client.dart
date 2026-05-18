6b12813d7e842b40712cf536668b6542158da98e
khaledzayeds <khaledtornado@gmail.com>
2026-05-18 13:47:07 +0300

    Make Flutter API client updateable before setup

--- QuickBooksFlutter/ledgerflow/lib/core/api/api_client.dart
+++ QuickBooksFlutter/ledgerflow/lib/core/api/api_client.dart
@@ -1,4 +1,5 @@
 import 'package:dio/dio.dart';
+
 import '../constants/app_constants.dart';
 import 'api_interceptors.dart';
 
@@ -11,22 +12,30 @@ class ApiClient {
   String? _token;
 
   void init({String? baseUrl}) {
-    if (_initialized) return;
+    final resolvedBaseUrl = _normalizeBaseUrl(
+      baseUrl ?? AppConstants.defaultBaseUrl,
+    );
+
+    if (_initialized) {
+      _dio.options.baseUrl = resolvedBaseUrl;
+      return;
+    }
+
     _dio = Dio(
       BaseOptions(
-        baseUrl:        baseUrl ?? AppConstants.defaultBaseUrl,
+        baseUrl: resolvedBaseUrl,
         connectTimeout: const Duration(seconds: 10),
         receiveTimeout: const Duration(seconds: 30),
         headers: {
           'Content-Type': 'application/json',
-          'Accept':       'application/json',
+          'Accept': 'application/json',
         },
       ),
     );
     _dio.interceptors.addAll([
       AppInterceptor(),
       LogInterceptor(
-        requestBody:  true,
+        requestBody: true,
         responseBody: true,
         logPrint: (obj) => debugLog(obj.toString()),
       ),
@@ -39,7 +48,18 @@ class ApiClient {
     return _dio;
   }
 
-  void updateBaseUrl(String baseUrl) => _dio.options.baseUrl = baseUrl;
+  void updateBaseUrl(String baseUrl) {
+    final resolvedBaseUrl = _normalizeBaseUrl(baseUrl);
+    if (!_initialized) {
+      init(baseUrl: resolvedBaseUrl);
+      return;
+    }
+    _dio.options.baseUrl = resolvedBaseUrl;
+  }
+
+  String get currentBaseUrl => _initialized
+      ? _dio.options.baseUrl
+      : AppConstants.defaultBaseUrl;
 
   /// Called by AuthNotifier after successful login
   void setToken(String token) {
@@ -55,8 +75,10 @@ class ApiClient {
 
   String? get currentToken => _token;
 
-  Future<Response<T>> get<T>(String path,
-          {Map<String, dynamic>? queryParameters}) =>
+  Future<Response<T>> get<T>(
+    String path, {
+    Map<String, dynamic>? queryParameters,
+  }) =>
       _dio.get<T>(path, queryParameters: queryParameters);
 
   Future<Response<T>> post<T>(String path, {dynamic data}) =>
@@ -70,4 +92,18 @@ class ApiClient {
 
   Future<Response<T>> delete<T>(String path, {dynamic data}) =>
       _dio.delete<T>(path, data: data);
+
+  static String _normalizeBaseUrl(String value) {
+    final trimmed = value.trim();
+    if (trimmed.isEmpty) return AppConstants.defaultBaseUrl;
+
+    final withScheme = trimmed.startsWith('http://') ||
+            trimmed.startsWith('https://')
+        ? trimmed
+        : 'http://$trimmed';
+
+    return withScheme.endsWith('/')
+        ? withScheme.substring(0, withScheme.length - 1)
+        : withScheme;
+  }
 }
